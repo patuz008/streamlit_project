6748e00008d0147d93ffbdea30595c290c886958
patuz008 <32542978+patuz008@users.noreply.github.com>
2024-11-08 13:34:37 +0100

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 import plotly.express as px
